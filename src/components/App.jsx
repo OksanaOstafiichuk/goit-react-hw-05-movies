74109a425d140bd5a5cb78bcde07f223a76b7b70
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-07-26 15:56:31 +0300

    Creates component routing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,27 @@
-import { Navigation } from './Navigation/Navigation';
+import { Routes, Route } from 'react-router-dom';
+
+import { Layout } from './Layout/Layout';
+import { Home } from '../pages/Home/Home';
+import { Movies } from '../pages/Movies/Movies';
+import { MovieDetails } from '../pages/MovieDetails/MovieDetails';
+import { Cast } from './Cast/Cast';
+import { Reviews } from './Reviews/Reviews';
+import { NotFound } from '../pages/NotFound/NotFound';
 
 export const App = () => {
   return (
     <div>
-      <Navigation />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="movies" element={<Movies />} />
+          <Route path="movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
