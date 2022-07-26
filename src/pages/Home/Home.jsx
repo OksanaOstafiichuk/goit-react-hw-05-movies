ec847fdbd1c28284dbb771baf62d8fdb0e01aaf5
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-07-26 18:23:47 +0300

    gets a movie search by keyword

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import * as API from '../../service/api-service';
 
 export const Home = () => {
   // API.fetchTrendingMovies().then(response => console.log(response));
+  // API.fetchMoviesByName().then(({ data }) => console.log(data.results));
   API.fetchMoviesById().then(({ data }) => console.log(data));
 
   return (
--- src/service/api-service.js
+++ src/service/api-service.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
 const API_KEY = '62788155badfe8d58c3d1f91f78357af';
-axios.defaults.baseURL = 'https://api.themoviedb.org';
+axios.defaults.baseURL = 'https://api.themoviedb.org/3';
 
 export const fetchTrendingMovies = () => {
-    const response = axios.get(`/3/trending/all/day?api_key=${API_KEY}`)
+    const response = axios.get(`/trending/all/day?api_key=${API_KEY}`)
+    return response;
+}
+
+
+//query - добавляти динамічно, подивитися чи є український переклад фільмів = uk
+export const fetchMoviesByName = () => {
+    const response = axios.get(`/search/movie?api_key=${API_KEY}&query='titanic'&language=en-US&page=1&include_adult=false`)
     return response;
 }
 
