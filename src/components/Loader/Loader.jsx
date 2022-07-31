62a2585ea2843e6e6b633736842ca17485ceb105
OksanaOstafiichuk <Ostafiichukk@gmail.com>
2022-07-31 18:19:31 +0300

    creates Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,9 @@
+import { Rings } from 'react-loader-spinner';
+
+export const Loader = () => {
+  return (
+    <div>
+      <Rings color="#00BFFF" height={80} width={80} />{' '}
+    </div>
+  );
+};
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,14 +4,18 @@ import { toast } from 'react-toastify';
 import * as API from '../../service/api-service';
 
 import { MoviesList } from 'components/MoviesList/MoviesList';
+import { Loader } from 'components/Loader/Loader';
 
 export const Home = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(null);
 
   useEffect(() => {
+    setIsLoading(true);
     API.fetchTrendingMovies()
       .then(({ data }) => {
         setTrendingMovies(data.results);
+        setIsLoading(false);
       })
       .catch(error => {
         toast.error(error.message);
@@ -21,6 +25,7 @@ export const Home = () => {
   return (
     <div>
       <h2>Trending Movies Today</h2>
+      {isLoading && <Loader />}
       <MoviesList movies={trendingMovies} />
     </div>
   );
